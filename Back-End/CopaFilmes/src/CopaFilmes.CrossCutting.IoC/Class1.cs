805cdaddd041450c4704370ec7de423112cc1509
Lucas Silva <luckkass89@gmail.com>
2021-02-03 19:05:19 -0300

    Criando estrutura do Back-End

---
+++ Back-End/CopaFilmes/src/CopaFilmes.CrossCutting.IoC/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CopaFilmes.CrossCutting.IoC
+{
+    public class Class1
+    {
+    }
+}
---
+++ Back-End/CopaFilmes/src/CopaFilmes.Infra.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CopaFilmes.Infra.Data
+{
+    public class Class1
+    {
+    }
+}
